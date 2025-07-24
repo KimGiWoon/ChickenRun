4acb911fd90a9f43f9de472eb8cf090738907eb8
frhug922 <dlsrnjsl3@gmail.com>
2025-07-24 16:56:07 +0900

    [Feat] Add Photon Fucntion
    Panel 컨트롤러 추가
     - MainSceneUIController
    
    View에 해당하는 Panel 스크립트 추가
     - PlayerSlot
     - PlayFindRoomPanel
     - PlayLobbyPanel
     - PlayMakeRoomPanel
     - RoomButton
    
    포톤매니저 추가
     - PhotonManager

---
+++ Assets/09.BIK_Folder/Scripts/PlayFindRoomPanel.cs
@@ -0,0 +1,95 @@
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayFindRoomPanel : UIBase
+{
+    #region Serialized fields
+
+    [Header("Room List UI")]
+    [SerializeField] private RectTransform _roomListRoot;
+    [SerializeField] private RoomButton _roomButtonPrefab;
+
+    [Header("Back Button")]
+    [SerializeField] private Button _backButton;
+
+    #endregion // Serialized fields
+
+
+
+
+
+    #region private fields
+
+    private List<RoomButton> _roomButtons = new();
+    private System.Action _onBack;
+
+    #endregion // private fields
+
+
+
+
+
+    #region mono funcs
+
+    private void Start()
+    {
+        _backButton.onClick.AddListener(() => _onBack?.Invoke());
+    }
+
+    #endregion // mono funcs
+
+
+
+
+
+    #region public funcs
+
+    public void Initialize(System.Action onBack)
+    {
+        _onBack = onBack;
+    }
+
+    public override void RefreshUI()
+    {
+        // 화면을 새로고침 할때마다 방 목록을 요청합니다.
+        PhotonManager.Instance.RequestRoomList(OnRoomListReceived);
+    }
+
+    #endregion // public funcs
+
+
+
+
+
+    #region private funcs
+
+    /// <summary>
+    /// 방 목록을 받아왔을 때 호출되는 콜백 함수입니다.
+    /// </summary>
+    /// <param name="roomList"></param>
+    private void OnRoomListReceived(List<RoomInfo> roomList)
+    {
+        RefreshRoomList(roomList);
+    }
+
+    private void RefreshRoomList(List<RoomInfo> rooms)
+    {
+        for (int i = 0; i < rooms.Count; i++) {
+            if (i >= _roomButtons.Count) {
+                RoomButton newBtn = Instantiate(_roomButtonPrefab, _roomListRoot);
+                _roomButtons.Add(newBtn);
+            }
+
+            _roomButtons[i].gameObject.SetActive(true);
+            _roomButtons[i].Initialize(rooms[i]);
+        }
+
+        for (int i = rooms.Count; i < _roomButtons.Count; i++) {
+            _roomButtons[i].gameObject.SetActive(false);
+        }
+    }
+
+    #endregion // private funcs
+}
