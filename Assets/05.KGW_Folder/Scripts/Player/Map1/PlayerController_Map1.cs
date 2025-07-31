735d7bd5eeb2a6c3505648314d6b84bb5c239224
frhug922 <dlsrnjsl3@gmail.com>
2025-07-31 11:50:41 +0900

    [!HOTFIX] Build Error Fix
    빌드 시 게임 동작이 안하는 문제 핫픽스

--- Assets/05.KGW_Folder/Scripts/Player/Map1/PlayerController_Map1.cs
+++ Assets/05.KGW_Folder/Scripts/Player/Map1/PlayerController_Map1.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
@@ -10,7 +10,7 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
     [SerializeField] PlayerState_Map1 _playerstate;
     [SerializeField] SpriteRenderer _playerRenderer;
     [SerializeField] Animator _playerAni;
-    
+
     [Header("Correction Setting")]
     [SerializeField] float _correctionValue = 15f;
 
@@ -36,12 +36,10 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
         _playerRigid = GetComponent<Rigidbody2D>();
 
         // 조종 가능 유/무에 따른 레이어 설정 (충돌 관련 세팅)
-        if (photonView.IsMine)
-        {
+        if (photonView.IsMine) {
             gameObject.layer = LayerMask.NameToLayer("Player");
         }
-        else
-        {
+        else {
             gameObject.layer = LayerMask.NameToLayer("RemotePlayer");
             // 반 투명화
             TranslucentSetting();
@@ -54,30 +52,46 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
         GameManager_Map1.Instance.StartPosSave(transform);
 
         // 자기 자신의 카메라 설정
-        if (photonView.IsMine)
-        {
+        if (photonView.IsMine) {
             Camera.main.GetComponent<CameraController>().SetTarget(transform);
+
+            if (GameManager_Map1.Instance == null) {
+                Debug.LogWarning("11111");
+            }
+
+            if (GameManager_Map1.Instance._gameUIManager == null) {
+                Debug.LogWarning("22222");
+
+            }
+            if (transform == null) {
+                Debug.LogWarning("33333");
+
+            }
+
+
+
             GameManager_Map1.Instance._gameUIManager.SetPlayerPosition(transform);
+
+
             _gameUIManager = GameManager_Map1.Instance._gameUIManager;
+
         }
     }
 
     private void Update()
     {
         // 자기 자신만 동작
-        if (photonView.IsMine)
-        {
+        if (photonView.IsMine) {
             PlayerStateUpdate();
             TouchInput();
             PlayerJump();
-        }       
+        }
     }
 
     private void FixedUpdate()
     {
         // 자신을 제외한 플레이어의 이동 위치 보간
-        if (!photonView.IsMine)
-        {
+        if (!photonView.IsMine) {
             transform.position = Vector3.Lerp(transform.position, _currentPosition, Time.fixedDeltaTime * _correctionValue);
             transform.rotation = Quaternion.Lerp(transform.rotation, _currentRotation, Time.fixedDeltaTime * _correctionValue);
         }
@@ -86,19 +100,22 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
     // 화면 터치 입력
     private void TouchInput()
     {
-        if (!_isGround) return;
+        if (!_isGround)
+            return;
 
         // 화면 터치 시 터치 타임 측정
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)) {
             // 옵션창 오픈 시 움직임 금지
-            if (_gameUIManager._isOptionOpen) return;
+            if (_gameUIManager._isOptionOpen)
+                return;
 
             // 이모티콘창 오픈 시 움직임 금지
-            if (_gameUIManager._isEmoticonPanelOpen) return;
+            if (_gameUIManager._isEmoticonPanelOpen)
+                return;
 
             // 결승점 통과 시 움직임 금지
-            if (GameManager_Map1.Instance._isGoal) return;
+            if (GameManager_Map1.Instance._isGoal)
+                return;
 
             _touchStartTime = Time.time;
             _isTouch = true;
@@ -108,15 +125,15 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
     // 플레이어 점프
     private void PlayerJump()
     {
-        if (!_isGround) return;
+        if (!_isGround)
+            return;
         // 화면 터치 끝
-        if (Input.GetMouseButtonUp(0) && _isGround && _isTouch)
-        {
+        if (Input.GetMouseButtonUp(0) && _isGround && _isTouch) {
             SoundManager.Instance.PlaySFX(SoundManager.Sfxs.SFX_Jump);
             _touchEndTime = Time.time - _touchStartTime;
             _currentAnimatorHash = Jump_Hash;
             _playerAni.Play(Jump_Hash);
-            
+
             // 0 -> 1의 값으로 부드럽게 점프 동작을 보정
             float maxTime = Mathf.Clamp01(_touchEndTime / _playerstate.MaxTouchTime);
             float jumpPower = Mathf.Lerp(_playerstate.JumpPower, _playerstate.MaxJumpPower, maxTime);
@@ -142,13 +159,11 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
     // 플레이어의 움직임에 대한 애니메이션
     private void PlayerStateUpdate()
     {
-        if (_playerRigid.velocity == Vector2.zero)
-        {
+        if (_playerRigid.velocity == Vector2.zero) {
             _currentAnimatorHash = Idle_Hash;
             _playerAni.Play(Idle_Hash);
         }
-        else if (_playerRigid.velocity.y < 0)
-        {
+        else if (_playerRigid.velocity.y < 0) {
             _currentAnimatorHash = Glide_Hash;
             _playerAni.Play(Glide_Hash);
         }
@@ -157,8 +172,7 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
     // 물리적 충돌
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")) {
             _isGround = true;
             _playerRigid.velocity = Vector2.zero;
         }
@@ -168,10 +182,8 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // 달걀 획득
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Egg"))
-        {
-            if (photonView.IsMine)
-            {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Egg")) {
+            if (photonView.IsMine) {
                 SoundManager.Instance.PlaySFX(SoundManager.Sfxs.SFX_GetEgg);
                 // 게임매니저의 알 개수 증가
                 GameManager_Map1.Instance.GetEgg(1);
@@ -180,8 +192,7 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
         }
 
         // 물에 입수
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Water"))
-        {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Water")) {
             SoundManager.Instance.PlaySFX(SoundManager.Sfxs.SFX_DropWater);
             Debug.Log("물에 접촉");
             // 게임의 처음 위치로 이동
@@ -190,31 +201,28 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
         }
 
         // 체크 포인트 접촉
-        if(collision.gameObject.layer == LayerMask.NameToLayer("CheckPoint"))
-        {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("CheckPoint")) {
             // 자신의 리스폰 위치 변경
-            if (photonView.IsMine)
-            {
+            if (photonView.IsMine) {
                 GameManager_Map1.Instance._startPos = collision.transform.position;
             }
         }
 
         // 결승점 도착
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Goal"))
-        {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Goal")) {
             // 골을 했으면 넘어가기
-            if (GameManager_Map1.Instance._isGoal) return;
+            if (GameManager_Map1.Instance._isGoal)
+                return;
 
             // 내 플레이어만 처리
-            if (photonView.IsMine)
-            {
+            if (photonView.IsMine) {
                 string playerNickname = PhotonNetwork.LocalPlayer.NickName;
                 SoundManager.Instance.PlaySFX(SoundManager.Sfxs.SFX_Goal);
 
                 // 도착을 알림
                 photonView.RPC(nameof(ArrivePlayer), RpcTarget.AllViaServer, playerNickname);
                 Debug.Log("결승선 도착");
-                
+
                 GameManager_Map1.Instance.StopStopWatch();
             }
         }
@@ -233,8 +241,7 @@ public class PlayerController_Map1 : MonoBehaviourPun, IPunObservable
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         // 동기화 데이터 보내기
-        if (stream.IsWriting)
-        {
+        if (stream.IsWriting) {
             stream.SendNext(transform.position);    // 플레이어의 위치 데이터 전송
             stream.SendNext(transform.rotation);    // 플레이어의 회전 데이터 전송
             stream.SendNext(_currentAnimatorHash);  // 현재 플레이어의 애니메이션 데이터 전송
