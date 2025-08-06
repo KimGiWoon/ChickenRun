c8cb94e7c1f199a8d977353e42871a3810cca0a3
01stone01 <hagwhr2@gmail.com>
2025-08-06 16:14:18 +0900

    [Feat] Add map2 game countdown
    선두가 들어왔을 때 카운트 다운 panel이 출력되도록 추가

--- Assets/06.LSW_Folder/Scripts/Map2/UI/BaseUI_Map2.cs
+++ Assets/06.LSW_Folder/Scripts/Map2/UI/BaseUI_Map2.cs
@@ -11,6 +11,7 @@ public class BaseUI_Map2 : MonoBehaviourPun
     [Header("Text UI Reference")]
     [SerializeField] TMP_Text _playTimeText;
     [SerializeField] TMP_Text _eggCountText;
+    [SerializeField] TMP_Text _endTimeText;
 
     [Header("Button UI Reference")]
     [SerializeField] Button _optionButton;
@@ -21,12 +22,16 @@ public class BaseUI_Map2 : MonoBehaviourPun
     [SerializeField] GameObject _emoticonPanel;
     [SerializeField] GameObject _defeatPanel;
     [SerializeField] GameObject _winPanel;
+    [SerializeField] GameObject _endTimePanel;
     
     [Header("Goal Slider UI Reference")]
     [SerializeField] Slider _playerPosSlider;
     
     private bool _isEmoticonPanelOpen;
     private bool _isSettingPanelOpen;
+
+    private Coroutine _endTimerRoutine;
+    private readonly int _timer = 60;
 
     private void Start()
     {
@@ -35,6 +40,10 @@ public class BaseUI_Map2 : MonoBehaviourPun
         GameManager_Map2.Instance.GameProgress.OnChanged += UpdateSlider;
         GameManager_Map2.Instance.OnWinGame += () => _winPanel.SetActive(true);
         GameManager_Map2.Instance.OnDefeatGame += () => _defeatPanel.SetActive(true);
+        GameManager_Map2.Instance.OnReachGoal += () =>
+        {
+            _endTimerRoutine = StartCoroutine(EndTimerCoroutine());
+        };
         
         _optionButton.onClick.AddListener(OnOptionPanel);
         _emoticonButton.onClick.AddListener(OnEmoticonPanel);
@@ -103,6 +112,22 @@ public class BaseUI_Map2 : MonoBehaviourPun
     private void UpdateSlider(float value)
     {
         _playerPosSlider.value = value;
+    }
+    
+    private IEnumerator EndTimerCoroutine()
+    {
+        WaitForSeconds time = new WaitForSeconds(1f);
+        _endTimePanel.SetActive(true);
+
+        for(int i = _timer; i >= 1; i--)
+        {
+            _endTimeText.text = i.ToString();
+            SoundManager.Instance.PlaySFX(SoundManager.Sfxs.SFX_Count);
+
+            yield return time;
+        }
+        _endTimePanel.SetActive(false);
+        _endTimerRoutine = null;
     }
 }
 
